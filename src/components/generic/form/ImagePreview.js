68a7fef1d6b1f2ed3a4cdc4a872ffd6824ea48fe
Paal Joergensen <pal.jorgensen@gmail.com>
2020-04-06 09:00:28 +0200

    HFP-2977 Add missing files

---
+++ src/components/generic/form/ImagePreview.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './ImagePreview.scss';
+
+const ImagePreview = ({src}) => {
+  return (
+    <span className="image-preview">
+      
+    </span>
+  );
+};
+
+/*ImagePreview.propTypes = {
+  src: PropTypes.string.isRequired,
+};*/
+
+export default ImagePreview;
---
+++ src/components/generic/message/Message.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Message.scss';
+
+const Message = ({severity, children}) => {
+  return (
+    <div className={`message ${severity}`}>
+      {children}
+    </div>
+  );
+};
+
+Message.propTypes = {
+  severity: PropTypes.string.isRequired,
+  children: PropTypes.string
+}
+
+export default Message;
